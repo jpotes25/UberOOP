ff6a454a226297fea434c66416f1492bd89852d6
Jdpotes25 <jdpotesp@ut.edu.co>
2021-03-13 16:50:54 -0500

    I'd add Uber types with oop in JavaScript

--- JS/index.js
+++ JS/index.js
@@ -3,3 +3,7 @@
 var car = new Car("VRC81D", new Account("David Potes", "1006116326"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("VRC81D", new Account("David Potes", "1006116326"), "Chevrolet", "Camaro");
+uberX.passenger = 4;
+uberX.printDataCar();
