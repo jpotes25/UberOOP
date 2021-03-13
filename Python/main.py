974b1046a49825184009e42e4595539cb8fa41f6
Jdpotes25 <jdpotesp@ut.edu.co>
2021-03-13 16:54:42 -0500

    Added some fixes to python classes

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,13 @@
 from car import Car
 from account import Account
+from uberX import UberX
 
 print("Hola Mundo")
 
 car = Car("VRC81D", Account("David Potes", "1006116326"))
 print(vars(car))
 print(vars(car.driver))
+
+uberX = UberX("VRC81D", Account("David Potes", "1006116326"), "Chevrolet", "Camaro")
+
+print(vars(uberX))
